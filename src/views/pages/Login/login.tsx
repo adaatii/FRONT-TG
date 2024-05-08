c3cec9e68ea988877e50f85b3efa9f9953d11a1b
adaatii <l.adati33@gmail.com>
2024-05-08 18:04:39 -0300

    Code: Changed name

--- src/views/pages/Login/login.tsx
+++ src/views/pages/Login/login.tsx
@@ -1,132 +1,135 @@
-import { useState } from 'react';
-import style from './login.module.scss'
-import { TextField, Button, Snackbar, Alert, Slide } from '@mui/material';
-import CookieManager from '../../../utils/CookieManager';
-import { useNavigate } from 'react-router-dom';
+import { useState } from "react";
+import style from "./login.module.scss";
+import { TextField, Button, Snackbar, Alert, Slide } from "@mui/material";
+import CookieManager from "../../../utils/CookieManager";
+import { useNavigate } from "react-router-dom";
 import { loginEmployee } from "../../../api/Hooks/login";
 
 const cookieManager = new CookieManager();
 
 function Login() {
-    const [openToast, setOpenToast] = useState(false);
+  const [openToast, setOpenToast] = useState(false);
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const [formData, setFormData] = useState({
-        email: '',
-        password: '',
-    });
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
 
-    const login = () => {
-        loginEmployee(formData).then((data) => {
-            if (data[0].token) {
-                cookieManager.Token = (data[0].token);
-                navigate('/main');
-            } else {
-                setOpenToast(true);
-            }
-        }).catch((error) => {            
-            console.log(error);
-            setOpenToast(true);
-        });
-
-    };
-
-    const handleClose = (
-        event: React.SyntheticEvent | Event,
-        reason?: string,
-    ) => {
-        if (reason === 'clickaway') {
-            return;
+  const login = () => {
+    loginEmployee(formData)
+      .then((data) => {
+        if (data[0].token) {
+          cookieManager.Token = data[0].token;
+          navigate("/main");
+        } else {
+          setOpenToast(true);
         }
-        setOpenToast(false);
-    };
+      })
+      .catch((error) => {
+        console.log(error);
+        setOpenToast(true);
+      });
+  };
 
-    return (
-        <div className={style.container}>
-            {openToast && (
-                <Snackbar
-                    open={openToast}
-                    autoHideDuration={3000}
-                    onClose={handleClose}
-                    anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
-                    TransitionComponent={Slide}
-                >
-                    <Alert severity="error" color="error">
-                        Email ou senha incorretos! Tente novamente.
-                    </Alert>
-                </Snackbar>
-            )}
-            <div className={style.left}>
-                <div className={style.header}>
-                    <span className={style.title}>Pantheon</span>
-                    <span className={style.subtitle}>Sign in to your account</span>
-                </div>
-                <div className={style.form}>
-                    <TextField
-                        fullWidth
-                        label="Email"
-                        variant="outlined"
-                        size="small"
-                        color="primary"
-                        inputProps={{ maxLength: 30 }}
-                        onChange={(e) => setFormData({ ...formData, email: e.target.value })}
-                        sx={{
-                            '& .MuiOutlinedInput-root': {
-                                '& fieldset': {
-                                    borderRadius: '12px ',
-                                },
-                                borderRadius: '12px ',
-                                '&:hover fieldset': {
-                                    borderColor: 'var(--primary)',
-                                },
-                            },
-                        }}
-                    />
-                    <TextField
-                        fullWidth
-                        label="Senha"
-                        type='password'
-                        variant="outlined"
-                        size="small"
-                        color="primary"
-                        inputProps={{ maxLength: 30 }}
-                        onChange={(e) => setFormData({ ...formData, password: e.target.value })}
-                        sx={{
-                            '& .MuiOutlinedInput-root': {
-                                '& fieldset': {
-                                    borderRadius: '12px ',
-                                },
-                                borderRadius: '12px ',
-                                '&:hover fieldset': {
-                                    borderColor: 'var(--primary)',
-                                },
-                            },
-                        }}
-                    />
-                    <Button
-                        fullWidth
-                        size="medium"
-                        variant="contained"
-                        color='primary'
-                        sx={{
-                            fontWeight: 'bold',
-                            color: 'var(--background)',
-                        }}
-                        onClick={login}
-                    >
-                        Login
-                    </Button>
+  const handleClose = (
+    event: React.SyntheticEvent | Event,
+    reason?: string,
+  ) => {
+    if (reason === "clickaway") {
+      return;
+    }
+    setOpenToast(false);
+  };
 
-                </div>
-                <div className={style.footer}>
-                    <span>powered by</span>
-                    <span className={style.name}>Pantheon</span>
-                </div>
-            </div>
-            <div className={style.right}>
-            </div>
+  return (
+    <div className={style.container}>
+      {openToast && (
+        <Snackbar
+          open={openToast}
+          autoHideDuration={3000}
+          onClose={handleClose}
+          anchorOrigin={{ vertical: "top", horizontal: "right" }}
+          TransitionComponent={Slide}
+        >
+          <Alert severity="error" color="error">
+            Email ou senha incorretos! Tente novamente.
+          </Alert>
+        </Snackbar>
+      )}
+      <div className={style.left}>
+        <div className={style.header}>
+          <span className={style.title}>Coffee</span>
         </div>
-    )
+        <div className={style.form}>
+          <TextField
+            fullWidth
+            label="Email"
+            variant="outlined"
+            size="small"
+            color="primary"
+            inputProps={{ maxLength: 30 }}
+            onChange={(e) =>
+              setFormData({ ...formData, email: e.target.value })
+            }
+            sx={{
+              "& .MuiOutlinedInput-root": {
+                "& fieldset": {
+                  borderRadius: "12px ",
+                },
+                borderRadius: "12px ",
+                "&:hover fieldset": {
+                  borderColor: "var(--primary)",
+                },
+              },
+            }}
+          />
+          <TextField
+            fullWidth
+            label="Senha"
+            type="password"
+            variant="outlined"
+            size="small"
+            color="primary"
+            inputProps={{ maxLength: 30 }}
+            onChange={(e) =>
+              setFormData({ ...formData, password: e.target.value })
+            }
+            sx={{
+              "& .MuiOutlinedInput-root": {
+                "& fieldset": {
+                  borderRadius: "12px ",
+                },
+                borderRadius: "12px ",
+                "&:hover fieldset": {
+                  borderColor: "var(--primary)",
+                },
+              },
+            }}
+          />
+          <Button
+            fullWidth
+            size="medium"
+            variant="contained"
+            color="primary"
+            sx={{
+              fontWeight: "bold",
+              color: "var(--background)",
+            }}
+            onClick={login}
+          >
+            Login
+          </Button>
+        </div>
+        <div className={style.footer}>
+          <span>powered by</span>
+          <span className={style.name}>Coffee</span>
+        </div>
+      </div>
+      <div className={style.right}></div>
+    </div>
+  );
 }
 export default Login;
+
